6a0dd32f0eb825ffc29bd853e126952a739e041c
Nestor <nestoruriarte23@gmail.com>
2020-06-09 21:21:46 +0200

    Now thing get rendered

--- CS300/CS300/src/Model/Model.cpp
+++ CS300/CS300/src/Model/Model.cpp
@@ -1016,8 +1016,8 @@ void Model::BindModelBuffer()
 	//adding tangents
 	glBindBuffer(GL_ARRAY_BUFFER, mVBO[4]);
 	glBufferData(GL_ARRAY_BUFFER, mTangents.size() * sizeof(glm::vec3), &mTangents[0].x, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(3);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 
 	if (mIndexed)
 	{
